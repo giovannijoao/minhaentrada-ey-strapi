9191403ad45acaa60dc4382ad5f8b779c504e624
João Giovanni <joaogiovannivilela@gmail.com>
2022-08-16 19:58:07 -0300

    Added middleware to prod

---
+++ config/env/production/middlewares.js
@@ -0,0 +1,28 @@
+module.exports = [
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'minhaentrada-ey-media.s3.us-east-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'minhaentrada-ey-media.s3.us-east-1.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+];
