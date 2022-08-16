3e4dea374772dd2d024819c1ca963f777b90512d
João Giovanni <joaogiovannivilela@gmail.com>
2022-08-16 08:42:52 -0300

    added schemas

---
+++ src/api/trilha/controllers/trilha.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  trilha controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::trilha.trilha');
---
+++ src/api/trilha/routes/trilha.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * trilha router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::trilha.trilha');
