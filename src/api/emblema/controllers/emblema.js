92c20e8fdf9715edcf1e4844d55fa8cae6a5b05b
João Giovanni <joaogiovannivilela@gmail.com>
2022-08-17 17:27:02 -0300

    Added features

---
+++ src/api/emblema/controllers/emblema.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  emblema controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::emblema.emblema');
---
+++ src/api/emblema/services/emblema.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * emblema service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::emblema.emblema');
