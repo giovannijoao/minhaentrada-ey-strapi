efaabe3a662520db360560eaa009acfc4dbbec6a
João Giovanni <joaogiovannivilela@gmail.com>
2022-10-07 19:59:12 -0300

    Conhecimentos

---
+++ src/api/conhecimento/controllers/conhecimento.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  conhecimento controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::conhecimento.conhecimento');
---
+++ src/api/conhecimento/services/conhecimento.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * conhecimento service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::conhecimento.conhecimento');
